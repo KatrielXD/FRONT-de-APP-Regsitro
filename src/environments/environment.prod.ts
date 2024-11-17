217647980b4e62d096f414af450cd99305798c55
KatrielXD <katrieldiaz@gmail.com>
2024-11-17 17:06:14 -0300

    Agrego link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: "backend-tasks-jet.vercel.app"
+    baseUrl: "https://backend-tasks-8w69qb29s-katriels-projects-83585a11.vercel.app"
 };
